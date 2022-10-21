0dc3d9784d44e7f86ba47772cd6b9c71af32c72a
henriqueyda <henrique.ydaa@gmail.com>
2022-10-21 01:11:33 -0300

    adiciona chamada de api

---
+++ src/Api.js
@@ -0,0 +1,30 @@
+import React, { useState} from 'react';
+
+function Api() {
+
+    const [variable, setVariable] = useState("Antes da chamada de API");
+
+    const getApiLocal = async () => {
+        await fetch("http://127.0.0.1:5000/v1/aula/consultar", { // Habilitei o CORS no backend e funcionou!
+            method: 'GET',
+        })
+            .then((res) => {
+                res.json().then((data) => {
+                    setVariable(data.mensagem)
+                })
+            }).catch((error) => console.log(error));
+
+    };
+    
+    return (
+        <div className="display-board">
+            <div className="btn">
+              <input type="text" id="nome"/>
+            <button type="button" onClick={(e) => getApiLocal()} className="btn btn-warning">Chamar API</button>
+            <p>{variable}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Api;
